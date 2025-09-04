b25b82e4853d4ffdfb3d4e9673f2f88b7c90ebe6
Th3mis25 <vmprpg@outlook.com>
2025-09-04 16:10:02 -0600

    ADD EJECUTIVO

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Este archivo puede ser reemplazado en el despliegue para ajustar la URL de la API.
 window.APP_CONFIG = {
   // URL del Web App de Google Apps Script.
-  API_BASE: 'https://script.google.com/macros/s/AKfycbxDVBTfb40aOeBZN0bGvy12VUg7OQbUEpPFz6jyieV57MUv9idwSV3d7B-zenZ4Weho/exec'
+  API_BASE: 'https://script.google.com/macros/s/AKfycbyDBNKnJgRyUqWTOEI8rbLQalgLQp6HSOaOIDw60QecSinQtZUtCUgdkGQwpiNV8nd0/exec'
 };
 
 // Valores sensibles como AUTH_USERS y API_TOKEN deben obtenerse desde un
