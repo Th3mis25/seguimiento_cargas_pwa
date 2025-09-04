4e50d86183537a6cb75fbefb4476b024ca7a699c
Th3mis25 <vmprpg@outlook.com>
2025-09-03 22:09:12 -0600

    actualizacion implemnetacion 3 sep

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Este archivo puede ser reemplazado en el despliegue para ajustar la URL de la API.
 window.APP_CONFIG = {
   // URL del Web App de Google Apps Script.
-  API_BASE: 'https://script.google.com/macros/s/AKfycbzwh8M5J7n0OUVHzROjd7TaDdfb5lMAMwm_3oRhUbIL53JSQZkijcy-6v3yPrTgUNMK/exec',
+  API_BASE: 'https://script.google.com/macros/s/AKfycby7nU6winC3yKXQp6PiNnFT2CHB4fytlCJcZ2ZexzbUlIUgfYxjZP6mnfGKAOqZf8A7/exec',
   // Usuarios autorizados para iniciar sesión en la app.
   AUTH_USERS: [
     { user: 'admin', password: '1234' },
