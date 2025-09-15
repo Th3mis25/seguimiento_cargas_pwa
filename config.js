abf3a7526875b52e7392a2a1e3804cc48aff9267
Th3mis25 <vmprpg@outlook.com>
2025-09-15 15:53:34 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Este archivo puede ser reemplazado en el despliegue para ajustar la URL de la API.
 window.APP_CONFIG = {
   // URL del Web App de Google Apps Script.
-  API_BASE: 'https://script.google.com/macros/s/AKfycbyDBNKnJgRyUqWTOEI8rbLQalgLQp6HSOaOIDw60QecSinQtZUtCUgdkGQwpiNV8nd0/exec',
+  API_BASE: 'https://script.google.com/macros/s/AKfycbwLoaOwsPTv5bXdFxx75lGfKEaeptsBrHHVG_hKTuO_PN1G5RtsUOxvW7cLF9J1L4dz/exec',
   // Archivo local con los usuarios autorizados y el token de API.
   SECURE_CONFIG_URL: 'secure-config.json'
 };
